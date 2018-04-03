4e2ff4d2783bbc104c55bc4f2cc69ecf18c9aa75
Yuji Ichikawa <ichikawa.yuji@gmail.com>
2018-04-03 09:10:25 +0900

    initial

---
+++ worker-rmi.js
@@ -0,0 +1,114 @@
+import ObjectID from 'bson-objectid';
+
+function getTransferList(obj, list = []) {
+    if (ArrayBuffer.isView(obj)) {
+        list.push(obj.buffer);
+        return list;
+    }
+    if (isTransferable(obj)) {
+        list.push(obj);
+        return list;
+    }
+    if (!(typeof obj === 'object')) {
+        return list;
+    }
+    for (const prop in obj) {
+        if (obj.hasOwnProperty(prop)) {
+            getTransferList(obj[prop], list);
+        }
+    }
+    return list;
+}
+
+function isTransferable(instance) {
+    const transferable = [ArrayBuffer];
+    if (typeof MessagePort !== 'undefined') {
+        transferable.push(MessagePort);
+    }
+    if (typeof ImageBitmap !== 'undefined') {
+        transferable.push(ImageBitmap);
+    }
+    return transferable.some(e => instance instanceof e);
+}
+
+export class WorkerRMI {
+    constructor(remote, ...args) {
+        this.remote = remote;
+        this.id = ObjectID().toString();
+        this.methodStates = {};
+        this.remote.addEventListener('message', event => {
+            const data = event.data;
+            if (data.id === this.id) {
+                this.returnHandler(data);
+            }
+        }, false);
+        this.constructorPromise = this.invokeRM(this.constructor.name, args);
+    }
+
+    invokeRM(methodName, args = []) {
+        if (!this.methodStates[methodName]) {
+            this.methodStates[methodName] = {
+                num: 0,
+                resolveRejects: {}
+            };
+        }
+        return new Promise((resolve, reject) => {
+            const methodState = this.methodStates[methodName];
+            methodState.num += 1;
+            methodState.resolveRejects[methodState.num] = { resolve, reject };
+            this.remote.postMessage({
+                id: this.id,
+                methodName,
+                num: methodState.num,
+                args
+            }, getTransferList(args));
+        });
+    }
+
+    returnHandler(data) {
+        const resolveRejects = this.methodStates[data.methodName].resolveRejects;
+        if (data.error) {
+            resolveRejects[data.num].reject(data.error);
+        } else {
+            resolveRejects[data.num].resolve(data.result);
+        }
+        delete resolveRejects[data.num];
+    }
+}
+
+
+async function handleWorkerRMI(event) {
+    const data = event.data;
+    const message = {
+        id: data.id,
+        methodName: data.methodName,
+        num: data.num,
+    };
+    let result;
+    if (data.methodName === this.name) {
+        this.workerRMI.instances[data.id] = new this(...data.args);
+        message.result = null;
+        this.workerRMI.target.postMessage(message, getTransferList(result));
+    } else {
+        const instance = this.workerRMI.instances[data.id];
+        if (instance) {
+            result = await instance[data.methodName].apply(instance, data.args)
+            message.result = result;
+            this.workerRMI.target.postMessage(message, getTransferList(result));
+        }
+    }
+}
+
+export function resigterWorkerRMI(target, klass) {
+    klass.workerRMI = {
+        target,
+        instances: {},
+        handler: handleWorkerRMI.bind(klass)
+    }
+    target.addEventListener('message', klass.workerRMI.handler);
+}
+
+export function unresigterWorkerRMI(target, klass) {
+    target.removeEventListener('message', klass.workerRMI.handler)
+    delete klass.workerRMI;
+}
